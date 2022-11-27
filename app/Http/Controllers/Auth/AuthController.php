8853e9212e23fd5c7830d561706db689de2f8de1
takuya_kajiwara <fd.elt.119@gmail.com>
2022-11-27 16:01:33 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class AuthController extends Controller
@@ -26,10 +27,37 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if(Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        //アカウントがロックされていたら弾く
+        $user = User::where('email', '=', $credentials['email'])->first();
 
-            return redirect()->route('home')->with('login_success', 'ログイン成功しました');
+        if(!is_null($user)) {
+            if($user->locker_flg === 1) {
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています'
+                ]);
+            }
+            if(Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                //成功したらエラーカウントを０にする
+                if($user->error_count > 0) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+                return redirect()->route('home')->with('login_success', 'ログイン成功しました');
+            }
+
+            //ログイン失敗したらエラーカウントを１増やす
+                $user->error_count = $user->error_count + 1;
+            //エラーカウントが６以上の場合はアカウントをロックする
+                if($user->error_count > 5) {
+                    $user->locked_flg = 1;
+                    $user->save();
+                    return back()->withErrors([
+                        'email' => 'アカウントがロックされました。解除したい場合は、運営者に連絡してください。'
+                    ]);
+
+                }
+                $user->save();
         }
         return back()->withErrors([
             'email' => 'メールアドレスかパスワードが間違っています'
